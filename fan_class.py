86548382a87ea54730d9d6361b63e82e009d6546
NicolasSKing <nocoben10@gmail.com>
2026-06-03 14:48:28 +0800

    and_now_i_made_the_last_two_getters_for_color_and_on_so_the_same_as_the_previous_getters_all_the_value_of_color_and_on_will_be_returned_here

--- fan_class.py
+++ fan_class.py
@@ -18,6 +18,11 @@ class Fan:
     def get_radius(self):
         return self.__radius
 
+    def get_color(self):
+        return self.__color
+
+    def get_on(self):
+        return self.__on
 
 
 
